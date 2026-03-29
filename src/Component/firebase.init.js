a6bea8a6898142dd2c9613d63fbee6f2c0361c3b
Mehedi Hassan <meh67719@gamil.com>
2026-03-30 00:22:38 +0600

    try to solve firebase api key problem

--- src/Component/firebase.init.js
+++ src/Component/firebase.init.js
@@ -6,7 +6,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
