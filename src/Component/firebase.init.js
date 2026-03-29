7d5eab618aa5590aaa68b711d4823007408656ee
Mehedi Hassan <meh67719@gamil.com>
2026-03-30 00:08:48 +0600

    try to solve deploy error

--- src/Component/firebase.init.js
+++ src/Component/firebase.init.js
@@ -6,7 +6,7 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
